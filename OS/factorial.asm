4dc37bf2b4704af5885befafa7d1214b1392dc19
권진한 <ymyhjh2@naver.com>
2022-03-22 19:56:03 +0900

    ~3/22 homework

--- OS/factorial.asm
+++ OS/factorial.asm
@@ -6,10 +6,9 @@ main:
 	li $v0 5
 	syscall
 	move $t0 $v0 
-	li $t1 1
+	li $a0 1
 	beq $t0 1 case1
 	jal fact
-	move $a0 $t1
 	li $v0 1
 	syscall
 	li $v0 10
@@ -26,7 +25,7 @@ case1:
 
 
 fact:
-	mul $t1 $t0 $t1
+	mul $a0 $t0 $a0
 	sub $t0 $t0 1
 	bne $t0 1 fact
 	jr $ra
